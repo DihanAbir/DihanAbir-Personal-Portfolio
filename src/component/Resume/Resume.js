a9a58a21777e8faa52c1fc9c4c891c2101775dc1
DihanAbir <nahid.muradabir@gmail.com>
2020-11-18 03:16:01 +0600

    side image spring effect added

--- src/component/Resume/Resume.js
+++ src/component/Resume/Resume.js
@@ -1,80 +1,99 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Resume.css';
+import "./Resume.css";
 
 const Resume = () => {
   return (
     <div classNameName="mx-auto ">
       <div>
-      <div className="resume">
-   
-  <div className="resume_right">
-    <div className="resume_item resume_about">
-        <div className="title">
-           <p className="bold">Who Am I?</p>
-         </div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis illo fugit officiis distinctio culpa officia totam atque exercitationem inventore repudiandae?</p>
-    </div>
-    <div className="resume_item resume_work">
-        <div className="title">
-           <p className="bold">Work Experience</p>
-         </div>
-        <ul>
-            <li>
-                <div className="date">2013 - 2015</div> 
-                <div className="info">
-                     <p className="semi-bold">Lorem ipsum dolor sit amet.</p> 
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
-                </div>
-            </li>
-            <li>
-              <div className="date">2015 - 2017</div>
-              <div className="info">
-                     <p className="semi-bold">Lorem ipsum dolor sit amet.</p> 
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
-                </div>
-            </li>
-            <li>
-              <div className="date">2017 - Present</div>
-              <div className="info">
-                     <p className="semi-bold">Lorem ipsum dolor sit amet.</p> 
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div className="resume_item resume_education">
-      <div className="title">
-           <p className="bold">Education</p>
-         </div>
-      <ul>
-            <li>
-                <div className="date">2010 - 2013</div> 
-                <div className="info">
-                     <p className="semi-bold">Web Designing (Texas University)</p> 
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
-                </div>
-            </li>
-            <li>
-              <div className="date">2000 - 2010</div>
-              <div className="info">
-                     <p className="semi-bold">Texas International School</p> 
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div className="resume_item resume_hobby">
-      <div className="title">
-           <p className="bold">Hobby</p>
-         </div>
-    </div>
-  </div>
-</div>
+        <div className="resume">
+          <div className="resume_right">
+            <div className="resume_item resume_about">
+              <div className="title">
+                <h4 className="bold">Who Am I?</h4>
+              </div>
+              <p>
+                A junior web developer who enthusiast the development of a
+                website. I have a 1-year working experience and I have completed
+                several web project. I always tried to find real-life problems
+                and try to find an efficient solution using my critical thinking
+                ability. Always I looking for a friendly environment where I can
+                learn so many things and also try to teach other people that
+                thing as well.
+              </p>
+            </div>
+            <div className="resume_item resume_work">
+              <div className="title">
+                <p className="bold">Work Experience</p>
+              </div>
+              <ul>
+                <li>
+                  <div className="date">2016 - 2018</div>
+                  <div className="info">
+                    <p className="semi-bold">Lorem ipsum dolor sit amet.</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, voluptatibus!
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="date">2015 - 2017</div>
+                  <div className="info">
+                    <p className="semi-bold">Lorem ipsum dolor sit amet.</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, voluptatibus!
+                    </p>
+                  </div>
+                </li>
+                {/* <li>
+                  <div className="date">2017 - Present</div>
+                  <div className="info">
+                    <p className="semi-bold">Lorem ipsum dolor sit amet.</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nostrum, voluptatibus!
+                    </p>
+                  </div>
+                </li> */}
+              </ul>
+            </div>
+            <div className="resume_item resume_education">
+              <div className="title">
+                <p className="bold">Education</p>
+              </div>
+              <ul>
+                <li>
+                  <div className="date">2010 - 2016</div>
+                  <div className="info">
+                    <p className="semi-bold">
+                      SSC & HSC 
+                    </p>
+                    <p>
+                      Academic School and College studies was completed from Dhaka Bangladesh!
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="date">2016 - present</div>
+                  <div className="info">
+                    <p className="semi-bold"> <b>Bangladesh University of Business & Technology</b> </p>
+                    <p>
+                      At Present, I am studing in Bangladesh University of Business & Technology, department of Computer Science and Technology.(Final semester)
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="resume_item resume_hobby">
+              <div className="title">
+                <p className="bold">Hobby</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-
-      
     </div>
   );
 };
--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -1,50 +1,97 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import profile from '../../image/profile.jpeg';
-import './Sidebar.css';
-import mypdf from '../../MD Nahid Murad Abir cv.pdf';
-import ReactTypingEffect from 'react-typing-effect';
+import React from "react";
+import { Link } from "react-router-dom";
+// import profile from "../../image/profile.jpeg";
+import { useSpring, animated } from "react-spring";
+import "./Sidebar.css";
+import mypdf from "../../MD Nahid Murad Abir cv.pdf";
+import ReactTypingEffect from "react-typing-effect";
 
+
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 const Sidebar = () => {
-    return (
-        <div className= 'sidebar-maindiv'>
-           
-            <hr/>
-            <div className="img">
-                <img className='img-fluid profile-picture' src={profile} alt="profile picture"/>
-            </div>
-            
-            <div className='personal-information'>
-                <p> 
-                <ReactTypingEffect
-        text={["Hello From Abir!", "A Enthusiast Developer", 'A Front-end Developer', 'React lover', 'Technology philander']}
-        cursorRenderer={cursor => <h1 style={ { color: 'salmon'}}>{cursor}</h1>}
-        displayTextRenderer={(text, i) => {
-          return (
-            <h4>
-              {text.split('').map((char, i) => {
-                const key = `${i}`;
-                return (
-                  <span
-                    key={key}
-                    style={ { color: 'salmon'}}
-                  >{char}</span>
-                );
-              })}
-            </h4>
-          );
-        }}        
-      />
-                </p>
-                <p> <span className="dot">o</span> <b>Birthday</b> : 02 Februar 1998</p>
-                <p> <span className="dot">o</span> <b>Job</b> : Freelancer</p>
-                <p> <span className="dot">o</span> <b>Email</b> : nahid.muradabir@gmail.com</p>
-                <p> <span className="dot">o</span> <b>LinkedIn</b> : dihanabir</p>
-            </div>
-                <button className='btn cv'><a href={mypdf} download="MD Nahid Murad Abir cv.pdf">Download CV</a></button>
-        </div>
-    );
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+  return (
+    <div className="sidebar-maindiv">
+      <hr />
+      <div className="img">
+        {/* <img
+          className="img-fluid profile-picture"
+          src={profile}
+          alt="profile picture"
+        /> */}
+        <animated.div
+          class="card"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{ transform: props.xys.interpolate(trans) }}
+        />
+      </div>
+
+      <div className="personal-information">
+        <p>
+          <ReactTypingEffect
+            text={[
+              "Hello From Abir!",
+              "A Enthusiast Developer",
+              "A Front-end Developer",
+              "React lover",
+              "Technology philander",
+            ]}
+            cursorRenderer={(cursor) => (
+              <h1 style={{ color: "salmon" }}>{cursor}</h1>
+            )}
+            displayTextRenderer={(text, i) => {
+              return (
+                <h4>
+                  {text.split("").map((char, i) => {
+                    const key = `${i}`;
+                    return (
+                      <span key={key} style={{ color: "salmon" }}>
+                        {char}
+                      </span>
+                    );
+                  })}
+                </h4>
+              );
+            }}
+          />
+        </p>
+        <p>
+          {" "}
+          <span className="dot">o</span> <b>Birthday</b> : 02 Februar 1998
+        </p>
+        <p>
+          {" "}
+          <span className="dot">o</span> <b>Job</b> : Freelancer
+        </p>
+        <p>
+          {" "}
+          <span className="dot">o</span> <b>Email</b> :
+          nahid.muradabir@gmail.com
+        </p>
+        <p>
+          {" "}
+          <span className="dot">o</span> <b>LinkedIn</b> : dihanabir
+        </p>
+      </div>
+      <button className="btn cv">
+        <a href={mypdf} download="MD Nahid Murad Abir cv.pdf">
+          Download CV
+        </a>
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
