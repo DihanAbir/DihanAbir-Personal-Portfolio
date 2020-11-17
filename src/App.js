d31dc7b3170d8893b91770ac29c243d00c890e11
DihanAbir <nahid.muradabir@gmail.com>
2020-11-18 02:12:13 +0600

    contact page and resume page create

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
      
          
       </Router>
-            <Footer/>
+            {/* <Footer/> */}
     </div>
   );
 }
--- src/component/Resume/Resume.js
+++ src/component/Resume/Resume.js
@@ -1,17 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Resume.css';
 
 const Resume = () => {
   return (
-    <div className="mx-auto ">
-      <div className="row">
-        <div className="col-12 mx-auto">
-          <h4>Preview My resume</h4>
-          <a className="btn btn-success" href="https://drive.google.com/file/d/1ggnewgLjEVEmcqhT-lwZw9GEhVfVm_hq/view">
-            Click Here
-          </a>
-        </div>
+    <div classNameName="mx-auto ">
+      <div>
+      <div className="resume">
+   
+  <div className="resume_right">
+    <div className="resume_item resume_about">
+        <div className="title">
+           <p className="bold">About us</p>
+         </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis illo fugit officiis distinctio culpa officia totam atque exercitationem inventore repudiandae?</p>
+    </div>
+    <div className="resume_item resume_work">
+        <div className="title">
+           <p className="bold">Work Experience</p>
+         </div>
+        <ul>
+            <li>
+                <div className="date">2013 - 2015</div> 
+                <div className="info">
+                     <p className="semi-bold">Lorem ipsum dolor sit amet.</p> 
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+                </div>
+            </li>
+            <li>
+              <div className="date">2015 - 2017</div>
+              <div className="info">
+                     <p className="semi-bold">Lorem ipsum dolor sit amet.</p> 
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+                </div>
+            </li>
+            <li>
+              <div className="date">2017 - Present</div>
+              <div className="info">
+                     <p className="semi-bold">Lorem ipsum dolor sit amet.</p> 
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div className="resume_item resume_education">
+      <div className="title">
+           <p className="bold">Education</p>
+         </div>
+      <ul>
+            <li>
+                <div className="date">2010 - 2013</div> 
+                <div className="info">
+                     <p className="semi-bold">Web Designing (Texas University)</p> 
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+                </div>
+            </li>
+            <li>
+              <div className="date">2000 - 2010</div>
+              <div className="info">
+                     <p className="semi-bold">Texas International School</p> 
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, voluptatibus!</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div className="resume_item resume_hobby">
+      <div className="title">
+           <p className="bold">Hobby</p>
+         </div>
+    </div>
+  </div>
+</div>
       </div>
+
+
       
     </div>
   );
